01cc3eaf4a519ced786c048e5d317c61becabbe6
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-05-21 11:02:55 +0200

    inloggning med admin fungerar nu

--- routes/authlogRoutes.js
+++ routes/authlogRoutes.js
@@ -6,7 +6,19 @@ const router = express.Router();
 
 //Login admin
 router.post("/login", async (req, res) => {
-console.log(" Inloggningssidan har ropats på ");
+    try {
+        const {username, password } = req.body;
+
+        //validate user and password
+        if(username ===`admin` && password ===`admin`) {
+            res.status(200).json({message: `Lyckad inloggning`, token: `här ska token placeras`});
+        } else {
+            res.status(401).json({message: `Fel användare eller lösenord`});
+        }
+
+    } catch (error) {
+        res.status(500).json({error: " serverfel"});
+    }
 });
 
 module.exports = router;
